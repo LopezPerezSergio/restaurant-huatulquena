bfbb817e28da02c2bfa1c6396bdfd7cd19474565
Sergio Lopez Perez <l17161158@tecnm.oaxaca.mx>
2023-05-02 17:34:15 -0500

    Correccion

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -146,17 +146,15 @@
             {{ route('roles.store') }}
         </x-slot:url>
 
-        <x-slot:form>
-            <div class="grid gap-4 mb-4 sm:grid-cols-2">
-                <div>
-                    <label for="nombre"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del rol</label>
-                    <input type="text" name="nombre" id="nombre"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Nombre" required="">
-                </div>
+        <div class="grid gap-4 mb-4 sm:grid-cols-2">
+            <div>
+                <label for="nombre"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del rol</label>
+                <input type="text" name="nombre" id="nombre"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Nombre" required="">
             </div>
-        </x-slot:form>
+        </div>
     </x-modal.create>
 
     @foreach ($roles as $rol)
